bb52765360679cf4be1eeab9b32092198eed9687
intrast <kovach.bohdan@ukr.net>
2019-01-29 23:26:35 +0200

    add model Profile

---
+++ TelegramBot/TelegramBot/Controllers/ProfilesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TelegramBot.Models;
+
+
+namespace TelegramBot.Controllers
+{
+    public class ProfilesController : Controller
+    {
+        Profile db = new Profile();
+        // GET: Profiles
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TelegramBot/TelegramBot/Models/Profile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelegramBot.Models
+{
+    public class Profile
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Skype { get; set; }
+        public string Email { get; set; }
+        public string LinkedIn { get; set; }
+        public int Work_experience { get; set; }
+        public string Skills { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+    }
+
+    public class ProfileDBContext : DbContext
+    {
+        public DbSet<Profile> Profiles { get; set; }
+    }
+}
